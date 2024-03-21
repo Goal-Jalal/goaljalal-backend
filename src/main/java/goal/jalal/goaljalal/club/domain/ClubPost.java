5906695558df53475e22453ac94a054d69979b78
wlswo <byunjin11@naver.com>
2024-03-21 23:37:09 +0900

    add : 클럽 게시글 도메인 추가

---
+++ src/main/java/goal/jalal/goaljalal/club/domain/ClubPost.java
@@ -0,0 +1,45 @@
+package goal.jalal.goaljalal.club.domain;
+
+import goal.jalal.goaljalal.global.domain.BaseEntity;
+import goal.jalal.goaljalal.member.domain.Member;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "clubPost")
+@Entity
+public class ClubPost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", columnDefinition = "text")
+    private String content;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public ClubPost(String content, Club club, Member member) {
+        this.content = content;
+        this.club = club;
+        this.member = member;
+    }
+}
