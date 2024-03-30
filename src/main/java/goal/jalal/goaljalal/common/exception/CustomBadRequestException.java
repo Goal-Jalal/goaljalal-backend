78e55d80a57a79233d9871af831a7a08804444b8
wlswo <byunjin11@naver.com>
2024-03-30 19:44:45 +0900

    feat : 커스텀 예외 클래스 생성

---
+++ src/main/java/goal/jalal/goaljalal/common/exception/CustomBadRequestException.java
@@ -0,0 +1,8 @@
+package goal.jalal.goaljalal.common.exception;
+
+public class CustomBadRequestException extends RuntimeException {
+
+    public CustomBadRequestException(final String message) {
+        super(message);
+    }
+}
