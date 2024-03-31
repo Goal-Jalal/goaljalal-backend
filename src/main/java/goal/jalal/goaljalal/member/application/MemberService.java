efae5f0c0aa715687425214a185fa57b21b0af93
wlswo <byunjin11@naver.com>
2024-03-31 23:41:27 +0900

    feat : member 정보 조회, 탈퇴 로직 추가

---
+++ src/main/java/goal/jalal/goaljalal/member/application/MemberService.java
@@ -0,0 +1,39 @@
+package goal.jalal.goaljalal.member.application;
+
+import goal.jalal.goaljalal.member.application.dto.MemberInfoResponse;
+import goal.jalal.goaljalal.member.configuration.dto.MemberKakaoIdDto;
+import goal.jalal.goaljalal.member.domain.Member;
+import goal.jalal.goaljalal.member.domain.MemberRepository;
+import goal.jalal.goaljalal.member.exception.member.MemberNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public MemberInfoResponse getMemberInformation(final MemberKakaoIdDto memberKakaoIdDto) {
+        final Long memberKakaoId = memberKakaoIdDto.kakaoId();
+
+        final Member member = memberRepository.findByKakaoId(memberKakaoId)
+            .orElseThrow(() -> new MemberNotFoundException(memberKakaoId));
+        return MemberInfoResponse.of(member);
+    }
+
+    public void leaveMember(final MemberKakaoIdDto memberKakaoIdDto) {
+        final Long memberKakaoId = memberKakaoIdDto.kakaoId();
+
+        final Member member = memberRepository.findByKakaoId(memberKakaoId)
+            .orElseThrow(() -> new MemberNotFoundException(memberKakaoId));
+
+        member.leave();
+        log.info(String.format("사용자 회원 탈퇴 - request info { kakaoId : %d }", memberKakaoId));
+    }
+}
---
+++ src/main/java/goal/jalal/goaljalal/member/application/dto/MemberInfoResponse.java
@@ -0,0 +1,20 @@
+package goal.jalal.goaljalal.member.application.dto;
+
+import goal.jalal.goaljalal.member.domain.Member;
+
+public record MemberInfoResponse(
+    Long id,
+    Long kakaoId,
+    String name,
+    String profileImageUrl
+) {
+
+    public static MemberInfoResponse of(final Member member) {
+        return new MemberInfoResponse(
+            member.getId(),
+            member.getKakaoId(),
+            member.getName().getValue(),
+            member.getProfileImageUrl()
+        );
+    }
+}
